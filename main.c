7f31349e7d40064d9d634184b667bb86b742d367
splo2t <yu990410@naver.com>
2018-05-31 21:29:59 +0900

    Initialize CAU_Calculate

---
+++ main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void calculate(int num1, char op, int num2);
+void Exitexamination(int a, char b, int c);
+void inputOperationExpression(int *num1, char *op, int *num2);
+
+int main() {
+
+   int num1, num2; //연산할 숫자
+   int op; //연산자
+   //int input_buffer[3];
+
+
+
+   while (1) {
+
+      inputOperationExpression(&num1, &op, &num2);
+
+      Exitexamination(num1, op, num2);
+
+      calculate(num1, op, num2);
+
+
+   }
+
+}
+
