97e344eb02b01d844d47e2e0be9a9b91a3f09af2
KBOHYUN <kbh5640@naver.com>
2018-05-31 21:53:46 +0900

    inputOperationExpression

--- main.c
+++ main.c
@@ -24,10 +24,17 @@ int main() {
 
 
    }
+
+}
+
+
 void Exitexamination(int a, char b, int c){
 
    if (a == 1 && b == '1' && c == 1) exit(0);
 
 }
-}
+void inputOperationExpression(int num1, char op, int num2){
+   printf("연산식을 입력하세요.(프로그램을 종료하시려면 1 1 1을 입력하세요.)");
+   scanf("%d %c %d", num1, op, num2);
 
+}
